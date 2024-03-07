e978cc69a069a4350fe24f95745ef32e1064185d
Pavel Sobolev <paveloomm@gmail.com>
2024-03-07 18:19:56 +0000

    Use the strictest preset for TypeScript.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,13 +1,21 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.ts',
-  mode: 'production',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+    entry: './src/index.ts',
+    mode: 'production',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
 }
