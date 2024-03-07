0908a884d6008852942175f7bfe45af1f8acf44c
Pavel Sobolev <paveloomm@gmail.com>
2024-03-07 21:04:57 +0000

    Add `webpack-dev-server`.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-    return <p>Hello there!</p>
+  return <p>Hello there!</p>;
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,26 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    entry: './src/index.tsx',
-    mode: 'production',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                loader: 'ts-loader',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx'],
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: 'src/index.html'
+  entry: './src/index.tsx',
+  mode: 'production',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
     })],
-}
+};
