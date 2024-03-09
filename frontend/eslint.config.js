68ed4e12a0c02cdc42e145045786132a686cf41f
Pavel Sobolev <paveloomm@gmail.com>
2024-03-09 10:57:43 +0000

    Set up ESLint Stylistic.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,17 +1,34 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
-export default tseslint.config({
-    ignores: ['dist/*'],
+const sources = {
+  files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+  ignores: ['dist/*'],
+};
+
+export default [
+  ...tseslint.config({
+    ...sources,
     extends: [
       eslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
     ],
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
     languageOptions: {
       parserOptions: {
         project: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
+  }),
+  {
+    ...sources,
+    ...stylistic.configs.customize({
+      semi: true,
+    }),
   },
-);
+];
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,4 +4,4 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const root = createRoot(document.getElementById('root')!);
-root.render(<StrictMode><App/></StrictMode>);
+root.render(<StrictMode><App /></StrictMode>);
