cd05a7afc06be916647b13ab5ec38eeb54708ce0
Pavel Sobolev <contact@paveloom.dev>
2024-04-11 21:51:41 +0300

    Extend linting for JSX files.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,9 @@
 import eslint from "@eslint/js";
 import pluginStylistic from "@stylistic/eslint-plugin";
 import pluginImport from "eslint-plugin-import";
+import pluginJsxA11y from "eslint-plugin-jsx-a11y";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginTypeScript from "typescript-eslint";
 
 const sources = {
@@ -72,6 +75,23 @@ export default [
         typescript: true,
         node: true,
       },
+    },
+  },
+  {
+    files: ["**/*.tsx"],
+    plugins: {
+      "react": pluginReact,
+      "react-hooks": pluginReactHooks,
+      "jsx-a11y": pluginJsxA11y,
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs["jsx-runtime"].rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      ...pluginJsxA11y.configs.strict.rules,
+    },
+    settings: {
+      react: { version: "detect" },
     },
   },
 ];
