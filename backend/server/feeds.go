cd9e961ab20cecd1428acd5785dbed3ae48d0cd3
Pavel Sobolev <contact@paveloom.dev>
2024-11-17 21:22:45 +0300

    Backend: add headers for CORS.

--- backend/server/feeds.go
+++ backend/server/feeds.go
@@ -26,10 +26,23 @@ func (handler *feedsHandler) handlePost(writer http.ResponseWriter, request *htt
 	fmt.Println(feed)
 }
 
+func (handler *feedsHandler) handleOptions(writer http.ResponseWriter, _ *http.Request) {
+	writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	writer.WriteHeader(http.StatusOK)
+}
+
 func (handler *feedsHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if origin := request.Header.Get("Origin"); origin != "" {
+		writer.Header().Set("Access-Control-Allow-Origin", origin)
+	}
+	writer.Header().Set("Vary", "Origin")
+
 	switch request.Method {
 	case http.MethodPost:
 		handler.handlePost(writer, request)
+	case http.MethodOptions:
+		handler.handleOptions(writer, request)
 	default:
 		writer.WriteHeader(http.StatusMethodNotAllowed)
 	}
