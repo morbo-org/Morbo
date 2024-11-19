72a9acf2e99d37afc8983ae1fd46dfd0d6313ca5
Pavel Sobolev <contact@paveloom.dev>
2024-11-19 22:33:31 +0300

    Backend: add the `users` and `sessions` tables to the database.

--- backend/db/migrations.go
+++ backend/db/migrations.go
@@ -16,4 +16,21 @@ var migrations = []migration{
 			INSERT INTO schema_version (version) VALUES (1);
 	    `,
 	},
+	{
+		version: 2,
+		sql: `
+			CREATE TABLE users(
+				id INTEGER NOT NULL PRIMARY KEY,
+				username TEXT NOT NULL UNIQUE,
+				password TEXT NOT NULL
+			);
+
+			CREATE TABLE sessions(
+				session_token TEXT NOT NULL PRIMARY KEY,
+				user_id INTEGER NOT NULL
+			);
+
+			UPDATE schema_version SET version = 2;
+		`,
+	},
 }
