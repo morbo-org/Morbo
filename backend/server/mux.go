bbf33fc8916fdcfd4eaa94361e55a368e152dbc8
Pavel Sobolev <contact@paveloom.dev>
2025-01-11 21:36:57 +0300

    Backend: implement the timeout middleware.

--- backend/server/mux.go
+++ backend/server/mux.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"fmt"
 	"net/http"
+	"sync/atomic"
+	"time"
 
 	"morbo/db"
 	"morbo/log"
@@ -12,6 +14,50 @@ import (
 type baseHandler struct {
 	db  *db.DB
 	log log.Log
+}
+
+type timeoutResponseWriter struct {
+	http.ResponseWriter
+	timedOut atomic.Bool
+}
+
+func (timeoutWriter *timeoutResponseWriter) WriteHeader(statusCode int) {
+	if timeoutWriter.timedOut.Load() {
+		return
+	}
+
+	timeoutWriter.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (timeoutWriter *timeoutResponseWriter) Write(b []byte) (int, error) {
+	if timeoutWriter.timedOut.Load() {
+		return 0, http.ErrHandlerTimeout
+	}
+
+	return timeoutWriter.ResponseWriter.Write(b)
+}
+
+func timeoutMiddleware(handler http.Handler) http.Handler {
+	f := func(writer http.ResponseWriter, request *http.Request) {
+		timeoutWriter := timeoutResponseWriter{ResponseWriter: writer}
+
+		done := make(chan struct{})
+		go func() {
+			handler.ServeHTTP(&timeoutWriter, request)
+			close(done)
+		}()
+
+		select {
+		case <-done:
+		case <-time.After(15 * time.Second):
+			timeoutWriter.timedOut.Store(true)
+
+			writer.WriteHeader(http.StatusGatewayTimeout)
+			writer.Write([]byte("took too long to finish the request"))
+		}
+	}
+
+	return http.HandlerFunc(f)
 }
 
 func (handler *baseHandler) newConnectionID() string {
@@ -25,11 +71,13 @@ func (handler *baseHandler) newConnectionID() string {
 
 func NewServeMux(db *db.DB) *http.ServeMux {
 	baseHandler := baseHandler{db, log.NewLog("handler")}
+	feedHandler := timeoutMiddleware(&feedHandler{baseHandler})
+	sessionHandler := timeoutMiddleware(&sessionHandler{baseHandler})
 
 	mux := http.ServeMux{}
 	mux.Handle("/{$}", http.NotFoundHandler())
-	mux.Handle("/feed/{$}", &feedHandler{baseHandler})
-	mux.Handle("/session/{$}", &sessionHandler{baseHandler})
+	mux.Handle("/feed/{$}", feedHandler)
+	mux.Handle("/session/{$}", sessionHandler)
 
 	return &mux
 }
