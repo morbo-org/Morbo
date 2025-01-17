534ef6939df6da8a582e0ac51100702d4151771d
Pavel Sobolev <contact@paveloom.dev>
2025-01-17 21:59:59 +0300

    Remove unneeded packages in the Nix flake.

--- flake.nix
+++ flake.nix
@@ -25,38 +25,14 @@
       devShells = forSystems (pkgs: {
         default = pkgs.mkShell {
           name = "morbo-shell";
-          nativeBuildInputs =
-            let
-              overrideGo =
-                pkg:
-                (pkg.override {
-                  buildGoModule = pkgs.buildGo123Module;
-                });
-            in
-            with pkgs;
-            [
-              bashInteractive
-              nixd
-              nixfmt-rfc-style
+          nativeBuildInputs = with pkgs; [
+            bashInteractive
+            nixd
+            nixfmt-rfc-style
 
-              ios-safari-remote-debug
-              ios-webkit-debug-proxy
-              nodejs_latest
-              typescript-language-server
-              vscode-langservers-extracted
-
-              go_1_23
-              (overrideGo gci)
-              (overrideGo gofumpt)
-              (overrideGo golangci-lint)
-              (overrideGo gopls)
-
-              bash-language-server
-              dockerfile-language-server-nodejs
-              hadolint
-              yamlfmt
-              yamllint
-            ];
+            yamlfmt
+            yamllint
+          ];
         };
       });
     };
